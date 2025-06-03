69906bcdc6f8e9c295942087791556df46c9b78d
Benjamin Teke <brumi1024@users.noreply.github.com>
2025-06-03 23:37:40 +0200

    Add eslint config.

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
+import { defineConfig } from "eslint/config";
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
-export default [
+export default defineConfig([
     eslintPluginUnicorn.configs.recommended,
   {
     rules: {
       'unicorn/better-regex': 'warn',
     },
   },
-];
+]);
