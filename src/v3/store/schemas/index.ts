0d8d9550c092560e62711c91c6696f84210f4f0c
Benjamin Teke <brumi1024@users.noreply.github.com>
2025-06-26 12:08:38 +0200

    Start fresh.

--- src/v3/store/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all schemas from this directory
-export * from './coreSchemas';
-export * from './changeSchemas';
-export * from './queueSchemas';
-export * from './propertySchemas';
-export * from './validationSchemas';
-export * from './nodeSchemas';
-export * from './apiSchemas';
